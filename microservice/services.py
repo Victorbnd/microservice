932e674e7805ca814295f925caf201b8adc3ccc1
Victorwls <victorbinda2017@hotmail.com>
2022-04-04 16:28:06 -0300

    env var creation correction

--- microservice/services.py
+++ microservice/services.py
@@ -4,7 +4,7 @@ def env_vars_list():
     return os.popen('env').read()
 
 def create_linux_env_var(env_name, env_var):
-    sheelEnv = env_name + '=' + env_var
+    sheelEnv = os.environ[str(env_name)] = str(env_var)
     os.popen('export '+ sheelEnv)
     return sheelEnv
 
